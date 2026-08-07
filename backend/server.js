aeab07f2fab6a2b82ae883bd585a85b809e6f18c
Prabina15 <prabinakathayat47@gmail.com>
2026-08-07 23:02:24 +0545

    completedd expense and dashboard part

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import mongoose from 'mongoose'
 import { connectDB } from './config/db.js'
 import userRouter from './routes/userRoute.js';
 import incomeRouter from "./routes/incomeRoute.js";
+import expenseRouter from "./routes/expenseRoute.js";
+import dashboardRouter from "./routes/dashboardRoute.js";
 
 const app = express();
 const port = 5000;
@@ -32,7 +34,8 @@ connectDB();
 // Routes
 app.use('/api/users', userRouter);
 app.use('/api/income', incomeRouter);
-
+app.use('/api/expense', expenseRouter);
+app.use('/api/dashboard', dashboardRouter);
 
 app.get('/', (req, res) => {
     res.send('API WORKING')
